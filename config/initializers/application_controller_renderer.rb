92be33fd1b484c1f878e790dfb86b1c9892ed7a6
tarotaron <suzu9095@gmail.com>
2020-02-03 15:55:23 +0900

    meshiterro finish

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0dcb80018bbb21eddb73e2397a8533fb20d52b560c5aa983c8849f777744a28b207e8514291040e343e9b75acfe19edaf844927a3fdc8a30295ecb81aaecb618'
